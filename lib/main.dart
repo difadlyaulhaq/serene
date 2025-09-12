c736bc979f97cd10d50c9fc81f96ad8dc3d5603d
difadlyaulhaq <difadlyaulhaq2@gmail.com>
2025-09-12 23:02:39 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:serene/login_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Serene',
+      home: const LoginPage(),
+      routes: {
+        '/login': (context) => const LoginPage(),
+        // '/chatscreen': (context) => const Placeholder(),
+      },
+    );
+  }
+}
+
---
+++ lib/shared/theme.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+Color white = const Color(0xFFFFFFFF);
+Color black = const Color(0xFF000000);
+Color pink = const Color(0xFFF573A0);
+Color teal = const Color(0xFF21AB88);
+Color blue = const Color(0xFF83A9F3);
+Color orange = const Color(0xFFEF883A);
+Color lavender = const Color(0xFFE8D5FF);
+Color lightPink = const Color(0xFFF0C4E1);
+Color peach = const Color(0xFFFFE4CC);
+Color darkGray = const Color(0xFF2C2C2C);
+Color softGray = const Color(0xFF666666);
+
+TextStyle headingStyle = TextStyle(
+  fontSize: 32,
+  fontWeight: FontWeight.bold,
+  color: black,
+);
+
+TextStyle subHeadingStyle = TextStyle(
+  fontSize: 18,
+  color: black,
+  fontWeight: FontWeight.w600
+);
